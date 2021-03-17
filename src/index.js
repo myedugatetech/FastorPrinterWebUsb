fa1dfe3154a573bb8bd48ee6f141cd0fe9774fca
Satbeer Singh <myedugatetech@gmail.com>
2021-03-17 20:21:07 +0530

    test function added

--- src/index.js
+++ src/index.js
@@ -1,21 +1,90 @@
 const WebUSB = require('./webusb');
 const EscPosEncoder = require('esc-pos-encoder');
 
-class FastorPrinterSdk extends EscPosEncoder{
-    constructor(){
+class FastorPrinterSdk extends EscPosEncoder {
+    constructor() {
         this.printer = new WebUSB();
     }
-    initialize(){
+    initialize() {
         super.initialize();
         this.printer.selectPrinter();
     }
-    async print(){
-        this.data = super.raw(['\x0a']).encode() ;
+    async print() {
+        this.data = super.raw(['\x0a']).encode();
         this.data = (new Uint8Array(this.data)).buffer
         await this.printer.open(console.log);
-        await this.printer.write(this.data,console.log)
+        await this.printer.write(this.data, console.log)
     }
-    close(){
+    printNonDineIn(printerName, order, note) {
+        
+        super.initialize(printerName)
+        super.align('center')
+            .line(order.restaurant_name)
+            .align('center')
+            .line(order.restaurant_address)
+            .align('center')
+            .line(order.user_name)
+            .align('center')
+            .line(order.user_mobile)
+            .line('--------------------------------------')
+            .line('Bill ref. ID: ')
+            .line(order.ref_orer_id)
+            .line('Date: ')
+            .line(order.date)
+            .line(order.time)
+            .line('Order No: ')
+            .line(order.order_no)
+            .line('Mode: ')
+            .line(order.mode)
+            .line('--------------------------------------');
+
+        order.f_order_items.map((value, index) => {
+            super.line(value.item_name)
+                .align('left')
+                .line(value.item_price)
+                .line('x')
+                .line(value.item_qty)
+                .align('right')
+                .line(value.item_q_total)
+        });
+
+        super.line('--------------------------------------')
+            .align('left')
+            .line('Total no. of Items')
+            .align('right')
+            .line(order.item_count)
+            .line('--------------------------------------')
+            .align('left')
+            .line('Payment Mode')
+            .align('right')
+            .line(order.txn_mode)
+            .align('left')
+            .line('Subtotal')
+            .align('right')
+            .line(order.subtotal)
+            .align('left')
+            .line('Discout')
+            .align('right')
+            .line(order.discount)
+            .align('left')
+            .line('Reward Coins Used')
+            .align('right')
+            .line(order.r_p_used)
+            .align('left')
+            .line('Taxes')
+            .align('right')
+            .line(order.taxes)
+            .align('left')
+            .line('Grand Total')
+            .align('right')
+            .line(order.grand_total)
+            .line('--------------------------------------')
+            .line('Powered by Fastor')
+            .line('Note:')
+            .line(note)
+            .print()
+    }
+    close() {
         return this.printer.close()
     }
 }
