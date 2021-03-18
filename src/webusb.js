4e7e7e89f1b2bc749832fdd3c67c33f44deae978
Satbeer Singh <myedugatetech@gmail.com>
2021-03-18 12:59:29 +0530

    bug fix

--- src/webusb.js
+++ src/webusb.js
@@ -19,6 +19,9 @@ class WebUSB {
             const printerFilters = [{ classCode: 0x07 }];
             this.device = await navigator.usb.requestDevice({ filters: printerFilters });
         }
+        if(!this.device){
+            throw new Error('PRINTER NOT FOUND')
+        }
     }
 
     async open(callback) {
