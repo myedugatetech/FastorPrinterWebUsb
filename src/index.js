cd97e59cd397da3f3f2b2517260c89217f4e3dcf
myedugatetech <55543025+myedugatetech@users.noreply.github.com>
2021-08-02 12:14:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const FastorWebUSBSdk = require('./webusb');
 const FastorLegacySdk = require("./legacy");
 
 module.exports = (type="LEGACY",paperwidth,characterPerLine) =>{
+		console.log("printing with "+ type);
 		if(type=="LEGACY") return new FastorLegacySdk(paperwidth,characterPerLine);
 		if(type=="USB") return new FastorWebUSBSdk(paperwidth,characterPerLine);
 		return new FastorWebUSBSdk(paperwidth,characterPerLine);
